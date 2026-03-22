7f303e417b3b8aceca21ee9d090e92cceba4d9dd
Karamjeet Sony <141219674+Aayush4532@users.noreply.github.com>
2026-03-22 22:24:32 +0530

    Update CORS to use Frontend_URI environment variable

--- server/main.go
+++ server/main.go
@@ -14,7 +14,7 @@ import (
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("Frontend_URI"));
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
